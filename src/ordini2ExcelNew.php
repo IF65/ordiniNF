187a6b015ccd58e3f7c76b3ee17427611d20fd30
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-07-13 10:58:00 +0200

    allineamento

--- src/ordini2ExcelNew.php
+++ src/ordini2ExcelNew.php
@@ -3,8 +3,8 @@
 
 	require '../vendor/autoload.php';
 	// leggo i dati da un file
-    $request = file_get_contents('/Users/if65/Desktop/Stilnovo/ordini.json');
-    //$request = file_get_contents('php://input');
+    //$request = file_get_contents('/Users/if65/Desktop/Stilnovo/ordini.json');
+    $request = file_get_contents('php://input');
     $data = json_decode($request, true);
 
     use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
@@ -490,3 +490,14 @@
     $writer = new Xlsx($workBook);
     $writer->save($file);
 
+	if (file_exists($file)) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.basename($file).'"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($file));
+		readfile($file);
+		exit;
+	}
