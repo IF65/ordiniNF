d4453a424019bac90ecf61ec9065127ceab4cc84
Marco Gnecchi <marco.gnecchi@icloud.com>
2020-04-29 18:44:03 +0200

    allineamento

--- src/buffaloControlloAE.php
+++ src/buffaloControlloAE.php
@@ -135,13 +135,15 @@ $sheet->setCellValueByColumnAndRow(2, 1, strtoupper('Descrizione'));
 $sheet->setCellValueByColumnAndRow(3, 1, strtoupper('Matricola'));
 $sheet->setCellValueByColumnAndRow(4, 1, strtoupper('Data'));
 $sheet->setCellValueByColumnAndRow(5, 1, strtoupper('Prog.'));
-$sheet->setCellValueByColumnAndRow(6, 1, strtoupper('Imposta'));
-$sheet->setCellValueByColumnAndRow(7, 1, strtoupper('Resi'));
-$sheet->setCellValueByColumnAndRow(8, 1, strtoupper('Resi QC'));
-$sheet->setCellValueByColumnAndRow(9, 1, strtoupper('Delta'));
-$sheet->setCellValueByColumnAndRow(10, 1, strtoupper('Totale'));
-$sheet->setCellValueByColumnAndRow(11, 1, strtoupper('Totale QC'));
-$sheet->setCellValueByColumnAndRow(12, 1, strtoupper('Delta'));
+$sheet->setCellValueByColumnAndRow(6, 1, strtoupper('Nat.'));
+$sheet->setCellValueByColumnAndRow(7, 1, strtoupper('Al.%'));
+$sheet->setCellValueByColumnAndRow(8, 1, strtoupper('Imposta'));
+$sheet->setCellValueByColumnAndRow(9, 1, strtoupper('Resi'));
+$sheet->setCellValueByColumnAndRow(10, 1, strtoupper('Resi QC'));
+$sheet->setCellValueByColumnAndRow(11, 1, strtoupper('Delta'));
+$sheet->setCellValueByColumnAndRow(12, 1, strtoupper('Totale'));
+$sheet->setCellValueByColumnAndRow(13, 1, strtoupper('Totale QC'));
+$sheet->setCellValueByColumnAndRow(14, 1, strtoupper('Delta'));
 
 $headerRowCount = 1;
 $row = $headerRowCount;
@@ -158,15 +160,18 @@ foreach ($data['righe'] as $riga) {
         $sheet->getCellByColumnAndRow( 4, $row )->getStyle()->getNumberFormat()->setFormatCode( NumberFormat::FORMAT_DATE_DDMMYYYY );
 
         $sheet->getCellByColumnAndRow( 5, $row )->setValueExplicit( $riga['progressivo'], DataType::TYPE_NUMERIC );
-        $sheet->getCellByColumnAndRow( 6, $row )->setValueExplicit( $riga['imposta'], DataType::TYPE_NUMERIC );
-        $sheet->getCellByColumnAndRow( 7, $row )->setValueExplicit( $riga['ammontareResi'], DataType::TYPE_NUMERIC );
-        $sheet->getCellByColumnAndRow( 8, $row )->setValueExplicit( $riga['ammontareResiQc'], DataType::TYPE_NUMERIC );
-        $formula = '=' . Coordinate::stringFromColumnIndex( 8 ) . "$row" . '-' . Coordinate::stringFromColumnIndex( 7 ) . "$row";
-        $sheet->getCellByColumnAndRow( 9, $row )->setValueExplicit( $formula, DataType::TYPE_FORMULA );
-        $sheet->getCellByColumnAndRow( 10, $row )->setValueExplicit( $riga['ammontare'], DataType::TYPE_NUMERIC );
-        $sheet->getCellByColumnAndRow( 11, $row )->setValueExplicit( $riga['ammontareQc'], DataType::TYPE_NUMERIC );
-        $formula = '=' . Coordinate::stringFromColumnIndex( 11 ) . "$row" . '-' . Coordinate::stringFromColumnIndex( 10 ) . "$row";
-        $sheet->getCellByColumnAndRow( 12, $row )->setValueExplicit( $formula, DataType::TYPE_FORMULA );
+        $sheet->getCellByColumnAndRow( 6, $row )->setValueExplicit( $riga['natura'], DataType::TYPE_STRING );
+        $sheet->getCellByColumnAndRow( 7, $row )->setValueExplicit( $riga['aliquotaIva'], DataType::TYPE_NUMERIC );
+
+        $sheet->getCellByColumnAndRow( 8, $row )->setValueExplicit( $riga['imposta'], DataType::TYPE_NUMERIC );
+        $sheet->getCellByColumnAndRow( 9, $row )->setValueExplicit( $riga['ammontareResi'], DataType::TYPE_NUMERIC );
+        $sheet->getCellByColumnAndRow( 10, $row )->setValueExplicit( $riga['ammontareResiQc'], DataType::TYPE_NUMERIC );
+        $formula = '=' . Coordinate::stringFromColumnIndex( 10 ) . "$row" . '-' . Coordinate::stringFromColumnIndex( 7 ) . "$row";
+        $sheet->getCellByColumnAndRow( 11, $row )->setValueExplicit( $formula, DataType::TYPE_FORMULA );
+        $sheet->getCellByColumnAndRow( 12, $row )->setValueExplicit( $riga['ammontare'], DataType::TYPE_NUMERIC );
+        $sheet->getCellByColumnAndRow( 13, $row )->setValueExplicit( $riga['ammontareQc'], DataType::TYPE_NUMERIC );
+        $formula = '=' . Coordinate::stringFromColumnIndex( 13 ) . "$row" . '-' . Coordinate::stringFromColumnIndex( 10 ) . "$row";
+        $sheet->getCellByColumnAndRow( 14, $row )->setValueExplicit( $formula, DataType::TYPE_FORMULA );
     }
 }
 
@@ -176,8 +181,9 @@ $highestRowIndex = $row + 1;
 $sheet->getStyle(Coordinate::stringFromColumnIndex(1).'1:'.Coordinate::stringFromColumnIndex(1)."$highestRowIndex")->getAlignment()->setHorizontal('center');
 $sheet->getStyle(Coordinate::stringFromColumnIndex(4).'1:'.Coordinate::stringFromColumnIndex(4)."$highestRowIndex")->getAlignment()->setHorizontal('center');
 $sheet->getStyle(Coordinate::stringFromColumnIndex(5).'1:'.Coordinate::stringFromColumnIndex(5)."$highestRowIndex")->getNumberFormat()->setFormatCode($integerFormat);
-$sheet->getStyle(Coordinate::stringFromColumnIndex(6).'1:'.Coordinate::stringFromColumnIndex(12)."$highestRowIndex")->getNumberFormat()->setFormatCode($currencyFormat);
-
+$sheet->getStyle(Coordinate::stringFromColumnIndex(6).'1:'.Coordinate::stringFromColumnIndex(6)."$highestRowIndex")->getAlignment()->setHorizontal('center');
+$sheet->getStyle(Coordinate::stringFromColumnIndex(7).'1:'.Coordinate::stringFromColumnIndex(7)."$highestRowIndex")->getNumberFormat()->setFormatCode($integerFormat);
+$sheet->getStyle(Coordinate::stringFromColumnIndex(8).'1:'.Coordinate::stringFromColumnIndex(14)."$highestRowIndex")->getNumberFormat()->setFormatCode($currencyFormat);
 $sheet->getStyle(Coordinate::stringFromColumnIndex(1).'1:'.Coordinate::stringFromColumnIndex($highestColumnIndex).'1')->getFont()->setBold(true);
 $sheet->getStyle(Coordinate::stringFromColumnIndex(1).'1:'.Coordinate::stringFromColumnIndex($highestColumnIndex).'1')->getAlignment()->setHorizontal('center');
 
